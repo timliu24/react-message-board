6b698ed96babf2823b2aaaaf3fc9b5f90a9c55e7
timliu24 <85093613+timliu24@users.noreply.github.com>
2022-05-05 17:19:37 -0700

    fixed controller

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,4 +5,5 @@ module.exports = (app) => {
     app.post('/api/users/login', UserController.login);
     app.post('/api/users/logout', UserController.logout);
     app.get('/api/users/:id', UserController.getOneUser);
+    
 };
--- server/server.js
+++ server/server.js
@@ -8,13 +8,10 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
-
 app.use(cors({origin: "http://localhost:3000"}))
 
 require("./config/mongoose.config");
 
 require("./routes/user.routes")(app);
-
-
 
 app.listen(process.env.MY_PORT, () => console.log(`Connected to port ${process.env.MY_PORT}`));
